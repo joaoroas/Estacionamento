9022e5431f09e19371d0dc3cd243107835b69357
Joao Roas <jr.roas@gmail.com>
2022-09-29 16:51:03 -0300

    initial

---
+++ Controllers/CarroController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Estacionamento.Controllers
+{
+    public class CarroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Retirar()
+        {
+            return View();
+        }
+    }
+}
